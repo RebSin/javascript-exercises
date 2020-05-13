8bd846a8c7207610bf826500a4afc4a1d5124e2f
Rebecca <rebeccasingh98@gmail.com>
2020-05-13 13:17:13 -0700

    Finished temp conversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fahrenVal) {
+//Deduct 32, then multiply by 5, then divide by 9
+    let finalFTOC = ((fahrenVal - 32) * 5) / 9;
+    return Math.round(finalFTOC * 10) / 10;
 
 }
 
-const ctof = function() {
-
+const ctof = function(celsiusVal) {
+//Divide by 5, then multiply by 9, then add 32
+  let finalCTOF = ((celsiusVal / 5) * 9) + 32;
+  return Math.round(finalCTOF * 10) /10;
 }
 
 module.exports = {

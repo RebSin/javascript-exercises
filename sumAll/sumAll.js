d3a02a3b7c3adcc748f111af5b4d3ea1d47f6229
Rebecca <rebeccasingh98@gmail.com>
2020-05-13 12:57:50 -0700

    Finished sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if (num1 > num2) {
+        let num3 = num1;
+        num1 = num2;
+        num2 = num3;
+    }
+    if (num1 < 0) {
+        return "ERROR";
+    }
+
+    if (typeof(num1) != "number" || typeof(num2) != "number"){
+        return "ERROR";
+    }
+
+    const firstNum = num1;
+    let finalNum = 0;
+    
+    for (let i = firstNum; i <= num2; i++) {
+        finalNum = finalNum + i;
+                  
+    }
+    return finalNum;
 
 }
 

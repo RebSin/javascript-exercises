46f99e62473043fd61f565684fc7467bee1bfc6f
Rebecca <rebeccasingh98@gmail.com>
2020-05-13 13:03:28 -0700

    Finished leap year exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(yearVal) {
+    if (yearVal % 4 == 0 && yearVal % 100 != 0) {
+        return true;
+    } 
+    if (yearVal % 100 == 0 && yearVal % 400 == 0) {
+        return true;
+    }
+    return false;
 
 }
 

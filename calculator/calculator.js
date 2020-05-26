8f02f68b8a6ab4c516ad24fb5db6abcf6b0e304a
Rebecca <rebeccasingh98@gmail.com>
2020-05-25 17:08:23 -0700

    Finished calculator inputs

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,40 @@
-function add () {
+function add (a, b) {
+	return a + b;
+}
+
+function subtract (a, b) {
+	return a - b;
 	
 }
 
-function subtract () {
+function sum (array) {
+	let total=0;
+	for (let i = 0; i < array.length; i++){
+		total += array[i];
+	}
+	return total;
 	
 }
 
-function sum () {
+function multiply (array) {
+	let total = 1;
+	for (let i = 0; i < array.length; i++){
+		total *= array[i];
+	}
+	return total;
 	
 }
 
-function multiply () {
-	
+function power(a, b) {
+	return a**b;
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(a) {
+	if (a == 0) {
+		a = 1;
+		return a;
+	}
+	return (a * (factorial(a -1)));
 }
 
 module.exports = {
